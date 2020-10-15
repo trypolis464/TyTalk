32f11ac12d4c5009c2abf57a15c4791b996e266d
TyGillespie <60036684+TyGillespie@users.noreply.github.com>
2020-10-15 07:40:19 -0600

    Update run.bat
    Optimized the code to be way smaller.

--- client/run.bat
+++ client/run.bat
@@ -1,8 +1,3 @@
-rem run.bat
-rem A part of the TyTalk Client.
-rem Copyright (C) 2020, Ty Gillespie. All rights reserved.
-rem Licensed under the MIT License.
-
 @echo on
 python -m http.server
 pause
