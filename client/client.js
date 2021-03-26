05d70c6c1b0038c45e26dc5ce5614cef6f3dbd5b
Ty <turret1226@gmail.com>
2021-03-26 15:28:58 -0600

    Removed the online and offline messages for now.

--- client/client.js
+++ client/client.js
@@ -4,16 +4,6 @@ const name = window.prompt("Enter your name.");
 const connection = new WebSocket('ws://localhost:8080');
 const button = document.querySelector('#send');
 
-connection.onopen = (event) => {
-  const data = "<p>" + name + " has connected.</p>";
-  connection.send(data);
-};
-
-connection.onclose = (event) => {
-  const data = "<p>" + name + " has disconnected.</p>";
-  connection.send(data);
-};
-
 connection.onerror = (event) => {
   console.error("WebSocket error observed:", event);
 };
