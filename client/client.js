bdb0e720e6dd1f108dc9254a758a83874c1f00d1
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-04-06 15:51:19 -0600

    Name is now an element on the page again, so you can change it easier.

--- client/client.js
+++ client/client.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const name = window.prompt("Enter your name.");
 const connection = new WebSocket("ws://localhost:8080");
 const button = document.querySelector("#send");
 
@@ -14,8 +13,9 @@ connection.onmessage = (event) => {
 };
 
 button.addEventListener("click", () => {
+  const name = document.querySelector("#name");
   const message = document.querySelector("#message");
-  const data = "<p>" + name + ": " + message.value + "</p>";
+  const data = "<p>" + name.value + ": " + message.value + "</p>";
   connection.send(data);
   message.value = "";
 });
